a85961a55d555d7d148aa531ca9b8fccc8e18ff1
Stan Peters <stanpetersmedia@hotmail.com>
2023-11-16 13:36:09 +0100

    overview toegevoegd van reports

--- src/components/Inspections.vue
+++ src/components/Inspections.vue
@@ -1,15 +1,57 @@
 <template>
     <div>
-        <h1 class="text-center">Uitgevoerde inspecties</h1>
-        <p class="text-center">hier komt een uitdraai van alle uitgevoerde inspecties</p>
-        <ul v-if="inspections.length > 0">
-            <li v-for="inspection in inspections" :key="inspection.id" class="text-center">
-                {{ inspection.id }}
-                {{ inspection.created }}
-            </li>
-        </ul>
-        <p class="text-center" v-else>Loading inspections...</p>
-    </div>
+        <h1 class="text-center mb-2" >Uitgevoerde inspecties</h1>
+        <v-card
+            v-for="inspection in inspections"
+            :key="inspection.id"
+            class="rounded border mx-auto elevation-12 pa-3"
+            width="fit-content"
+        >
+            <div
+                v-for="report in inspection"
+                :key="report.id"
+            >
+                <h2>Gegevens rapportage ID: {{ report.id }}</h2>
+                <div class="border rounded my-2 pa-2 elevation-2">
+                    <p>Aangemaakt: {{ report.created }} </p>
+                </div>
+                <h4>Schade opnemen:</h4>
+                <div class="border rounded my-2 pa-2 elevation-2">
+                    <p>Locatie: {{ report.damageNotation.location }}</p>
+                    <p>Nieuwe schade: {{ report.damageNotation.newDamage }}</p>
+                    <p>Soort schade: {{ report.damageNotation.damageSort }}</p>
+                    <p>Datum: {{ report.damageNotation.date }}</p>
+                    <p>Acute actie vereist: {{ report.damageNotation.urgentToRepair }}</p>
+                    <p>Omschrijving: {{ report.damageNotation.description }}</p>
+                </div>
+                <h4>Achterstallig onderhoud opnemen:</h4>
+                <div class="border rounded my-2 pa-2 elevation-2">
+                    <p>Locatie: {{ report.deferredMaintenance.location }}</p>
+                    <p>Soort onderhoud: {{ report.deferredMaintenance.maintenanceSort }}</p>
+                    <p>Acute actie vereist : {{ report.deferredMaintenance.urgentToMaintain }}</p>
+                    <p>Kostenindicatie: &euro;{{ report.deferredMaintenance.costEstimate }}</p>
+                </div>
+                <h4>Technische installatie inspecteren:</h4>
+                <div class="border rounded my-2 pa-2 elevation-2">
+                    <p>Locatie: {{ report.inspectionTechnicalInstallations.location }}</p>
+                    <p>Soort installatie: {{ report.inspectionTechnicalInstallations.installationType }}</p>
+                    <p>Gemelde storing: {{ report.inspectionTechnicalInstallations.reportedMalfunction }}</p>
+                    <p>Testprocedure link: {{ report.inspectionTechnicalInstallations.testProcedure }}</p>
+                    <p>Goedgekeurd: {{ report.inspectionTechnicalInstallations.approved }}</p>
+                    <p>Opmerkingen: {{ report.inspectionTechnicalInstallations.description }}</p>
+                </div>
+                <h4>Modificaties inventariseren:</h4>
+                <div class="border rounded my-2 pa-2 elevation-2">
+                    <p>Bestaande siutatie en reeds gedocumenteerde modificaties (link): {{ report.inventorizeModifications.currentSituationAndAlreadyDocumentedModifications }}</p>
+                    <p>Locatie aangetroffen modificatie: {{ report.inventorizeModifications.locationModification }}</p>
+                    <p>Uitgevoerd door: {{ report.inventorizeModifications.modificationMadeBy }}</p>
+                    <p>Beschrijving modificatie: {{ report.inventorizeModifications.descriptionOfModification }}</p>
+                    <p>Te ondernemen actie: {{ report.inventorizeModifications.actionToBeTaken }}</p>
+                    <p>Opmerkingen: {{ report.inventorizeModifications.description }}</p>
+                </div>
+            </div>
+        </v-card>
+        </div>
 </template>
 
 <script>
@@ -41,6 +83,7 @@ methods: {
         const data = await response.json()
         console.log('fetched data: ', data);
         this.inspections = data;
+        console.log(data);
         } catch (error) {
             console.log('Error when fetching JSON', error);
         } 
